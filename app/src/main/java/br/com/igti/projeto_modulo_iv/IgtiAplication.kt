8782f2cda49d7f07e5f8a67f2d9efd5e05e651c3
@gabrielmarinhodossantoscd <gabrielmarinhodossantoscd@gmail.com>
2022-06-25 16:12:21 -0300

    Adicionando Retrofit, OKhtpp e configurando Logs

---
+++ app/src/main/java/br/com/igti/projeto_modulo_iv/IgtiAplication.kt
@@ -0,0 +1,42 @@
+package br.com.igti.projeto_modulo_iv
+
+import android.app.Application
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class IgtiAplication : Application() {
+
+    companion object{
+        lateinit var retrofit: Retrofit
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(15L, TimeUnit.SECONDS)
+            .readTimeout(15L, TimeUnit.SECONDS)
+            .writeTimeout(15L, TimeUnit.SECONDS)
+            .addInterceptor(HttpLoggingInterceptor{ msg ->
+                println("LOG APP: $msg")
+            }.apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }).addNetworkInterceptor(HttpLoggingInterceptor{ msg ->
+                println("LOG NTW: $msg")
+            }.apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https:igti.com.br")
+            .client(okHttpClient)
+            .build()
+
+    }
+
+}
