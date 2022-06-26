aaeca7a305ba76c15b06db5c9a7684bb34c77c50
@gabrielmarinhodossantoscd <gabrielmarinhodossantoscd@gmail.com>
2022-06-26 16:46:25 -0300

    Criando metosdo de Alunos na viewModel

--- app/src/main/java/br/com/igti/projeto_modulo_iv/viewmodel/AlunoViewModel.kt
+++ app/src/main/java/br/com/igti/projeto_modulo_iv/viewmodel/AlunoViewModel.kt
@@ -1,6 +1,28 @@
 package br.com.igti.projeto_modulo_iv.viewmodel
 
 import androidx.lifecycle.ViewModel
+import br.com.igti.projeto_modulo_iv.data.remote.dto.AlunoRequestDTO
 
 class AlunoViewModel : ViewModel() {
+
+    fun listarAlunos(){
+        //TODO Repository.get()
+    }
+
+    fun listarAlunoPorId(id:String){
+        //TODO repository.getById(id)
+    }
+
+    fun cadastrarAluno(alunoRequestDTO: AlunoRequestDTO){
+        //TODO repository.post(aluno)
+    }
+
+    fun alterarAlunos(id:String, aluno :AlunoRequestDTO){
+        //TODO repository.put(id, Aluno)
+    }
+
+    fun excluirAluno(id: String){
+        //TODO repository.delete(id)
+    }
+
 }
